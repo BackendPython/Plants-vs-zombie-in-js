829cdcd44c2b491f6a1d8dc674974b0fea24a789
backendpython <holmirzaevsohruh196@gmail.com>
2022-08-31 15:57:31 +0500

    main

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let plants = document.querySelectorAll('.plants')
+
+
+let plants_1_details = {
+    heal: 10,
+    power_attack: 10,
+    open_class: "plants-1-open",
+    close_class: "plants-1-close",
+}
+
+
+let mainChecking = setInterval(() => {
+    
+}, 10);
