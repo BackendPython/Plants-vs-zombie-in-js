3c0b2deedaeae6982e801996ac0f27bd29f6090b
backendpython <holmirzaevsohruh196@gmail.com>
2022-08-31 19:13:49 +0500

    main

--- js/main.js
+++ js/main.js
@@ -2,23 +2,22 @@ let plants_shop = document.querySelectorAll('.box')
 let grounds = document.querySelectorAll('.ground')
 let pointsBox = document.querySelector('.points')
 
-console.log(plants_shop);
 
 // game informations and main settings
 let game_components = {
-    points: 80,
+    points: 50,
     gameStart: true,
 }
 
-let plants_1_details = {
+let peo = {
     heal: 10,
     power: 100,
     power_attack: 10,
 }
 
-let plants_2_details = {
+let sun = {
     heal: 5,
-    power: 70,
+    power: 50,
     recover_time: 3000,
     open_class: "plants-2-open",
     close_class: "plants-2-close",
@@ -26,17 +25,36 @@ let plants_2_details = {
 
 
 let mainChecking = setInterval(() => {
+    plants_shop.forEach(function(plant){
     if (game_components.gameStart==true) {
         pointsBox.textContent = game_components.points
-        if (game_components.points>plants_1_details.power-10) {
-            plants_shop.forEach(function(plant){
-                if (plant.classList=='box plants-1') {
+        if (game_components.points>peo.power-10) {
+                if (plant.classList=='box peo') {
                     plant.classList.add('open')
                 }
-            })
+        }
+        if (game_components.points>sun.power-10) {
+                if (plant.classList=='box sun') {
+                    plant.classList.add('open')
+                }
         }
     }
+    })
 }, 10);
+
+plants_shop.forEach(function(plant){
+    plant.addEventListener('click', function(){
+        plants_shop.forEach(function(down){
+            down.classList.remove('top')
+        })
+        plant.classList.add('top')
+    })
+})
+
+
+
+
+// point set
 setTimeout( function points() {
     game_components.points = game_components.points + 10
     setTimeout(() => {
