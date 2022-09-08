9074b7c3414dbaad9654746775c237c91995e0f5
backendpython <holmirzaevsohruh196@gmail.com>
2022-09-08 13:56:32 +0500

    main

---
+++ test.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+num1 = tk.Entry()
+
+
+
+root.title('My app')
+root.maxsize(width=1200, height=750)
+root.minsize(width=500, height=300)
+root.config(bg='green')
+
+root.mainloop()
